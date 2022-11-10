8cb6b5fd55bb9ca3f16209c07aa7407b57f65e78
Alexandr Aladin <kowalski@DESKTOP-NF9MK8E.localdomain>
2022-11-11 01:42:17 +0300

    Lab4: A*

--- Lab4_OOP/World.cpp
+++ Lab4_OOP/World.cpp
@@ -2,10 +2,12 @@
 
 Cell::Cell () : _x(0), _y(0), _lock(false), _distance()
 {
+
 }
-Cell::Cell (int x, int y, bool lock) : _x(x), _y(y), _lock(lock)
+Cell::Cell (int x, int y) : _x(x), _y(y), _lock(false)
 {
     _distance = abs(x) + abs(y);
+    _go_to = nullptr;
 }
 void Cell::SetX (int x)
 {
@@ -15,6 +17,10 @@ void Cell::SetY (int y)
 {
     _y = y;
 }
+void Cell::SetDistance ()
+{
+
+}
 void Cell::SetLock ()
 {
     _lock = true;
@@ -23,10 +29,10 @@ void Cell::SetUnlock ()
 {
     _lock = false;
 }
-void Cell::SetDistance ()
-{
-
-}
+//int Cell::GetId() const
+//{
+//    return _id;
+//}
 int Cell::GetX () const
 {
     return _x;
@@ -43,18 +49,73 @@ int Cell::ManhattanDistance() const
 {
     return abs(_x) + abs(_y);
 }
-void Cell::A_star()
+std::vector <Cell*> Cell::GetNeighbors (World & world) const
 {
-    std::priority_queue <std::pair <int, int>> frontier;
+    std::vector <Cell*> neighbors;
+    for (int i = _x - 1; i <= _x + 1 ; i += 2)
+    {
+        for (int j = _y - 1; j <= _y + 1; j += 2)
+        {
+            if (!world._field[i][j]->_lock)
+            {
+                neighbors.push_back(world._field[i][j]);
+            }
+        }
+    }
+    return neighbors;
 }
+void Cell::A_star (World & world)
+{
+    std::priority_queue <std::pair <int, Cell*>, std::vector <std::pair <int, Cell*>>, std::greater<>> frontier;
+    std::map <Cell*, Cell*> came_from;
+    std::map <Cell*, int> cost_so_far;
+    frontier.push(std::make_pair(0, this));
+    came_from[this] = this;
+    cost_so_far[this] = 0;
+    while (!frontier.empty())
+    {
+        std::pair current = frontier.top();
+        if (current.second->_x == 0 && current.second->_y == 0)
+        {
+            break;
+        }
+        for (Cell* next : this->GetNeighbors(world))
+        {
+            int cost = cost_so_far[current.second];
+            if (!cost_so_far.count(next) || cost < cost_so_far[next])
+            {
+                cost_so_far[next] = cost;
+                int priority = cost + next->ManhattanDistance();
+                frontier.emplace(priority, next);
+                came_from[next] = current.second;
+            }
+        }
+    }
+}
+
+//World::World (int size)
+//{
+//    _size = size;
+//    int x = -size, y = -size;
+//    for (int i = 0; i < (size + 1) * (size + 1); ++i, ++x)
+//    {
+//        if (i == size)
+//        {
+//            x = -size;
+//            ++y;
+//        }
+//        Cell new_cell(x, y, i);
+//        _field.emplace(new_cell.GetId(), &new_cell);
+//    }
+//}
 
 World::World (int size)
 {
     _size = size;
-    _field.reserve(size);
-    for (int i = 0; i < size; ++i)
+    _field.reserve(2 * size + 1);
+    for (int i = 0; i < 2 * size + 1; ++i)
     {
-        _field[i].reserve(size);
+        _field[i].reserve(2 * size + 1);
     }
 }
 void World::DistanceUpdate ()
--- Lab4_OOP/World.h
+++ Lab4_OOP/World.h
@@ -6,6 +6,7 @@
 
 class Warrior;
 class Building;
+class World;
 
 class Cell
 {
@@ -14,30 +15,34 @@ private:
     int _y;
     bool _lock;
     int _distance;
-    int _id;
+//    int _id;
     Cell* _go_to;
 public:
     Cell ();
-    Cell (int x, int y, bool lock);
+    Cell (int x, int y);
     void SetX (int x);
     void SetY (int y);
     void SetDistance ();
     void SetLock ();
     void SetUnlock ();
+//    [[nodiscard]] int GetId () const;
     [[nodiscard]] int GetX () const;
     [[nodiscard]] int GetY () const;
     [[nodiscard]] int GetDistance () const;
     [[nodiscard]] int ManhattanDistance () const;
-    void A_star ();
+    std::vector <Cell*> GetNeighbors (World & world) const;
+    void A_star (World & world);
 };
 
 class World
 {
 private:
     int _size;
-    std::vector <std::vector <Cell>> _field;
+    std::vector <std::vector <Cell*>> _field;
     std::map <int, Building*> _buildings;
     std::map <int, Warrior*> _entities;
+    friend std::vector <Cell*> Cell::GetNeighbors (World & world) const;
+//    friend class Cell;
 public:
     explicit World (int size);
     void DistanceUpdate ();
